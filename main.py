c2aac7b1d7c1becfc6b22785b259060e44d29899
jinawalic <jinawatitus387@gmail.com>
2025-11-23 18:14:01 +0100

    duplicate visitor done

--- main.py
+++ main.py
@@ -16,6 +16,13 @@ def get_last_visitor():
     pass
 
 def add_visitor(visitor_name):
+    # --- Duplicate visitor logic added here ---
+    last_name, last_time = get_last_visitor()
+
+    if last_name == visitor_name:
+        raise DuplicateVisitorError(f"{visitor_name} already visited.")
+    # ------------------------------------------
+
     pass
 
 def main():
